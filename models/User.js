8b67e52dfd60f28762c2162e992789f31391e4db
HP Nguyen <phucvovinam97@gmail.com>
2023-07-23 13:02:15 +0700

    Change method get acronym name in welcome message

--- models/User.js
+++ models/User.js
@@ -7,5 +7,12 @@ class User {
     this.username = username;
     this.password = password;
   }
+  getAcronymName() {
+    let fullName = `${this.lastName} ${this.firstName}`;
+    fullName = fullName.split(' ');
+    const firstName = fullName.pop();
+    const acronym = fullName.reduce((result, word) => result + word[0].toUpperCase(), '');
+    return firstName + acronym;
+  }
 }
 
--- scripts/home.js
+++ scripts/home.js
@@ -4,16 +4,9 @@ const welcomeMessage = document.querySelector('#welcome-message');
 const loginModal = document.querySelector('#login-modal');
 const logoutBtn = document.querySelector('#btn-logout');
 
-function getWelcomeName() {
-  let welcomeName = `${currentUser.lastName} ${currentUser.firstName}`;
-  welcomeName = welcomeName.split(' ');
-  const firstName = welcomeName.pop();
-  const acronym = welcomeName.reduce((result, word) => result + word[0], '');
-  return firstName + acronym;
-}
 if (currentUser) {
-  const welcomeName = getWelcomeName();
-  welcomeMessage.textContent = `Welcome, ${welcomeName}`;
+  const logonUser = new User(...Object.values(currentUser));
+  welcomeMessage.textContent = `Welcome, ${logonUser.getAcronymName()}`;
   loginModal.style.display = 'none';
 } else {
   loginModal.style.display = 'block';
